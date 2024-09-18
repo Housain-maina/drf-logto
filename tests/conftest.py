1798bd60718790cb2fde78412420eaa77e5c3936
Hussaini Usman <hussainmaina27@gmail.com>
2024-09-18 08:56:49 +0100

    change test client scope to function

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,6 +2,6 @@ import pytest
 from rest_framework.test import APIClient
 
 
-@pytest.fixture(scope="session")
+@pytest.fixture(scope="function")
 def test_client() -> APIClient:
     return APIClient()
